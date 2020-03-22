50ee35d1b766d9d7b11051ded448ab62cd85a342
muhannadjudah <muhannadjudah@gmail.com>
2020-03-22 22:29:00 +0100

    add search list files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+// import { List, Segment } from 'semantic-ui-react'
 import './App.css';
 
 function App() {
+
+
+  const list = [{ name: "Muhannad" }, { name: "Mohannad" }, { name: "Muhammad" }, { name: "Mohammad" }, { name: "Fida" }, { name: "Shadi" }, { name: "Hadi" }, { name: "Sima" }, { name: "Hani" }, { name: "razan" }, { name: "Hiba" }, { name: "hana" }, { name: "fadi" }]
+  const [res, setRes] = useState(list)
+
+  const updateList = (event) => {
+    setRes(list.filter(ele => ele.name.toLocaleLowerCase().indexOf(event.target.value) !== -1))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="ui inverted segment">
+        <div role="list" className="ui divided inverted relaxed list">
+          {res.map((item, index) => (
+            <div key={index} role="listitem" className="item">
+              <div className="content">{item.name} </div>
+            </div>
+          )
+          )}
+        </div>
+      </div>
+
+      <div className="ui icon input">
+        <input onChange={updateList} type="text" name="name" placeholder="Search..." />
+        <i aria-hidden="true" className="search icon"></i>
+      </div>
+
     </div>
   );
 }
